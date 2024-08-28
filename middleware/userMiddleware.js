91191271bd2e609f8690aa47477ce20c2df0df23
John Estes <john.r.estes@icloud.com>
2024-08-28 08:48:20 -0500

    added basePath

--- middleware/userMiddleware.js
+++ middleware/userMiddleware.js
@@ -2,7 +2,9 @@
 
 const userMiddleware = (req, res, next) => {
     res.locals.user = req.user;
+    res.locals.basePath = process.env.BASE_PATH || ''; // Ensure basePath is available in all views
     next();
 };
 
 export default userMiddleware;
+
