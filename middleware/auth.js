540da4936fd4fde729b1f65769e72841f485410e
John Estes <john.r.estes@icloud.com>
2024-08-28 21:58:35 -0500

    base path removed from auth route

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,12 @@
 // middleware/auth.js
+
+const BASE_PATH = process.env.BASE_PATH || ''; // Use the BASE_PATH environment variable
+
 const authMiddleware = (req, res, next) => {
     if (req.isAuthenticated()) {
         return next();
     }
-    res.redirect(`${BASE_PATH}/auth/google`); // Redirect to login if not authenticated
+    res.redirect(`/auth/google`); // Redirect to login if not authenticated
 };
 
 export default authMiddleware;
